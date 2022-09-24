0764c076d4587edd7819cade869acd22b8229ae5
Cesar Perales <cesar.perales.a24@gmail.com>
2022-09-24 11:45:34 -0500

    el osciladorArmonico.py ya genera el archivo datos.txt :D

--- osciladorArmonico.py
+++ osciladorArmonico.py
@@ -17,18 +17,62 @@ Experimento (8 puntos)
     e. Guarde el gráfico en formato png con un nombre descriptivo de forma automática (su código debe realizar este paso).
 """
 
-# Definir Constantes
-k = 500
+def tabulacion(base_datos, esp=2):
 
-# Pedir valor de masa (en gramos) para tensar la cuerda
-mGramos = input("Ingrese un valor entero entre 500 y 1000: ")
-m = int(mGramos) * (10 ** (-3))
+    # Definimos listas para añadir la cantidad de caracteres de
+    # todos los elementos y de cada columna
+    lis_max = [esp for x in range(len(base_datos[0]))]
+    for x in range(len(base_datos)):
+      for i in range(len(base_datos[x])):
+        if len(base_datos[x][i]) > lis_max[i]-esp:
+          lis_max[i] = len(base_datos[x][i])+esp
 
-# Calcular los parametros del oscilador
-w = np.sqrt(k/m)
-f = w/(2*np.pi)
+    # Ahora automatizamos hacer la cadena de .format
 
-print("La frecuencia de oscilación del sistema es: ", f)
+    tabulador = ""
+    for fil in range(len(base_datos)):
+        for colu in range(len(lis_max)):
+            tabulador += ("{:<" + str(lis_max[colu]) + "} ").format(
+                base_datos[fil][colu])
+        tabulador += ("\n" if fil != 0 else "\n" + ((len(tabulador) - 3) * "-") + "\n")
+    pass
 
-# Helloooooo
-print("Hello World")
+  
+    # Por ultimo reemplazamos los datos para que sea tabulado
+    return tabulador
+pass
+
+
+data = [["masa(kg)", "w", "Frecuencia(Hz)"]];
+
+for x in range(int(input("Cuantos datos va a ingresar: "))):
+
+  # Definir Constantes
+  k = 500
+
+  # Pedir valor de masa (en gramos) para tensar la cuerda
+  mGramos = input("\nIngrese un valor entero entre 500 y 1000: ")
+  m = int(mGramos) * (10 ** (-3))
+
+  # Calcular los parametros del oscilador
+  w = np.sqrt(k/m)
+  f = w/(2*np.pi)
+
+  data.append([str(m),str(w),str(f)]);
+  print("La frecuencia de oscilación del sistema es: ", f)
+  print()
+
+  pass
+
+#Escribir datos
+
+with open("datos.txt", "w") as dataFile:
+  for x in data:
+    for y in x:
+      dataFile.write(y+";")
+      pass
+    dataFile.write("\n")
+    pass
+
+print("Datos guardados con exito!\n")
+print(tabulacion(data))
