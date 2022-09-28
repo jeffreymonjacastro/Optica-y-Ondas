bca1579742ad10a9bc57704ed8a0329b3b7a1036
Cesar Perales <cesar.perales.a24@gmail.com>
2022-09-28 09:23:25 -0500

    leer archivo

--- funciones.py
+++ funciones.py
@@ -1,5 +1,11 @@
-def hola():
-  return "hola"
-
+def read(nombre):
+  with open(nombre, "r") as document:
+    data = document.readlines();
+    for x in data:
+      x = x.split(";")
+      pass
+  return data
 
 # HELOOOOOOOOOOOOO
+
+print(read("datos.txt"))
--- osciladorArmonico.py
+++ osciladorArmonico.py
@@ -9,8 +9,9 @@ Experimento (8 puntos)
   3. Anote la frecuencia a la que oscila el sistema a 2 decimales.
   4. Repetir los pasos del 1 al 3 para 10 diferentes valores de masa. Todos los valores deben ser distintos y deben diferir entre ellos en por lo menos 10 gramos.
   5. Guarde sus datos en un archivo de texto.
-  6. Elabore un programa que:
-    a. Lea el archivo de texto generado en el paso 5. 
+  6. Elabore un programa que:0
+    a. 
+    Lea el archivo de texto generado en el paso 5. 
     b. Grafique la frecuencia de oscilación del sistema vs la masa del oscilador armónico.
     c. Aplique un ajuste. El tipo de ajuste utilizado queda a su criterio y será parte de la evaluación.
     d. Imprima en la línea de comandos los parámetros del ajuste y el tipo de ajuste utilizado.
@@ -52,11 +53,14 @@ for x in range(int(input("Cuantos datos va a ingresar: "))):
 
   # Pedir valor de masa (en gramos) para tensar la cuerda
   mGramos = input("\nIngrese un valor entero entre 500 y 1000: ")
-  m = int(mGramos) * (10 ** (-3))
+  m = float(mGramos)/1000
 
   # Calcular los parametros del oscilador
   w = np.sqrt(k/m)
   f = w/(2*np.pi)
+
+  w = round(w,2)
+  f = round(f,2)
 
   data.append([str(m),str(w),str(f)]);
   print("La frecuencia de oscilación del sistema es: ", f)
