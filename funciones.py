b386ebf539cbb36febc05f496e1150a24594a39d
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2022-09-30 17:07:50 -0500

    Update main

--- funciones.py
+++ funciones.py
@@ -1,5 +1,8 @@
 import numpy as np
-import math
+import mathplotlib.pyplot as plt
+
+
+
 def read(nombre):
   with open(nombre, "r") as document:
     data = document.readlines();
@@ -9,6 +12,35 @@ def read(nombre):
   return data
 
 
-def frecuencia(m):
-  f = np.sqrt(500/m)/(2*np.pi)
-  return f
+def f(n):
+    if n == 0:
+        return 15
+    f = np.sqrt(500 / n) / (2 * np.pi)
+    return f
+
+
+def scatter(data):
+    X = []
+    Y = []
+
+    for i in range(1, len(data)):
+      X.append(float(data[i][0]))
+      Y.append(float(data[i][2]))
+
+    # Ajuste
+    x = np.arange(0.5, 1.01, 0.1)
+    y = [f(i) for i in x]
+    plt.plot(x, y, color="b")
+
+    # Datos dispersos
+    plt.scatter(X, Y, color="black")
+    plt.title("Frecuencia vs masa")
+    plt.xlabel("Masa")
+    plt.ylabel("Frecuencia")
+
+    # Guardar la gráfica
+    plt.savefig("grafica.png")
+    plt.show()
+
+
+scatter(data)
