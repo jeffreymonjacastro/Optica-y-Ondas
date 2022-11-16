0e100f5c3e98cad879009bb60a05d4b9260528cb
Cesar Perales <cesar.perales.a24@gmail.com>
2022-11-16 10:36:03 -0500

    no corre

---
+++ Proyecto experimental/refraccionParte1.py
@@ -0,0 +1,37 @@
+from funciones import *
+import math
+
+def leySnell(angulo_incidente, n1, n2):
+    return (n1*math.sin(angulo_incidente)/n2);
+
+
+def graficadora_refraccion1(data):
+    p_x = [];
+    p_y = [];
+
+    # Ingresando los datos del archivo de texto
+    for i in range(1, len(data)):
+        p_x.append(math.sin(float(data[i][0])));
+        p_y.append(math.sin(float(data[i][1])));
+
+    x = np.array(p_x);
+    y = np.array(p_y);
+
+    res, cov = curve_fit(leySnell,x,y);
+
+    print(res);
+    print(cov);
+
+# Leer datos desde el archivo creado
+refraccion1_datos = read("refraccion1_data.txt")
+
+
+print("Caso 1:")
+print("="*20)
+print()
+
+# Imprimiendo los datos del archivo creado
+print(tabulacion(refraccion1_datos))
+
+graficadora_refraccion1(refraccion1_datos);
+
